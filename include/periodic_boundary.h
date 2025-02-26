c2b6702f1770f55a40981bfc608f00766b3ea980
hhafiya <149326005+hhafiya@users.noreply.github.com>
2025-02-26 15:02:23 +0200

    periodic boundary conditions added

---
+++ include/periodic_boundary.h
@@ -0,0 +1,20 @@
+#ifndef PERIODIC_BOUNDARY_H
+#define PERIODIC_BOUNDARY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define N 2000
+#define X_SIZE 50.0
+#define Y_SIZE 50.0
+#define SIGMA 1.0
+#define SQUARE_SIZE 1.0
+#define RADIUS (SQUARE_SIZE / sqrt(2))
+#define GRID_SIZE 10.0
+typedef struct Particle
+{
+    double x, y;
+} Particle;
+Particle particles[N];
+#endif //PERIODIC_BOUNDARY_H
